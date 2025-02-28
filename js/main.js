55f4c42f28604fee39aaaab6083537ec1c83bbce
Pavlyshyn Ivanna <ivannapavl28@gmail.com>
2025-02-28 03:25:26 +0200

    stylized and thead

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function includeHTML(file, elementId) {
+  fetch(file)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(elementId).innerHTML = data;
+    })
+    .catch((error) => console.error(`Error loading ${file}:`, error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  includeHTML("components/header.html", "header-placeholder");
+  includeHTML("components/sidebar.html", "sidebar-placeholder");
+
+  let currentPath = window.location.pathname.split("/").pop();
+  const observer = new MutationObserver(() => {
+    const navItems = document.querySelectorAll(".nav-item");
+
+    if (navItems.length > 0) {
+      navItems.forEach((link) => {
+        if (link.getAttribute("href") === "./" + currentPath) {
+          link.classList.add("active");
+        } else {
+          link.classList.remove("active");
+        }
+      });
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+});
+
+function checkTh() {
+  const checkbox = document.getElementById("checkbox1");
+  if (checkbox.checked) {
+    checkbox.checked = false;
+  }
+}
