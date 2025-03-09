1827d338283a4b9ff1011da224c8603ae6dfd6bb
Pavlyshyn Ivanna <ivannapavl28@gmail.com>
2025-03-09 19:13:15 +0200

    all functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const getElement = document.querySelector.bind(document);
 
-
 let addIS = false;
 let editIs = false;
 const select_group = getElement("#select-group");
@@ -9,7 +8,8 @@ const l_name = getElement("#lname");
 const select_gender = getElement("#select-gender");
 const input_date = getElement("#input-date");
 
-
+let clickedElemToDel = null;
+let all = false;
 
 class Student {
   static count = 2;
@@ -22,16 +22,12 @@ class Student {
     this.birthday = birthday;
     this.status = status;
   }
-
-
 }
 
 const students = [];
 let student = null;
 
-
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   loadPage();
 });
 
@@ -56,17 +52,6 @@ function changeDisplayBlock(elem) {
   elem.style.display =
     getComputedStyle(elem).display !== "block" ? "block" : "none";
 }
-
-function checkTh() {
-  const checkbox = getElement("#checkbox1");
-  if (checkbox.checked) checkbox.checked = false;
-}
-
-function openDropProfile() {
-  changeDisplayFlex(getElement("#drop-profile"));
-}
-
-
 
 function openAdd() {
   addIS = true;
@@ -105,11 +90,81 @@ function closeAdd() {
   changeDisplayFlex(getElement("#addStudent"));
 }
 
+function closeDel() {
+  changeDisplayFlex(getElement("#wrapper-shadow"));
+  changeDisplayFlex(getElement("#delete-wraper"));
+  clickedElemToDel = null;
+  all = false;
+}
+
+function openDelOne() {
+  openDel();
+  all = false;
+  clickedElemToDel = event.target;
+  const studentId = clickedElemToDel.dataset.studentId;
+
+  const studentIndex = students.findIndex((student) => student.id == studentId);
+  if (studentIndex !== -1) {
+    getElement(
+      "#war_text"
+    ).textContent = `Are you sure you want to delete user ${students[studentIndex].name}`;
+  }
+}
+
+function openDel() {
+  changeDisplayFlex(getElement("#wrapper-shadow"));
+  changeDisplayFlex(getElement("#delete-wraper"));
+}
+
 function createElem(tag) {
   return document.createElement(`${tag}`);
 }
 
+function butAvai(elem) {
+  let parentTd = elem.parentElement.parentElement.children[6];
 
+  console.log(document.querySelectorAll('input[type="checkbox"]'));
+  let but_edit = parentTd.querySelector("#button-edit");
+  let but_del = parentTd.querySelector("#button-del");
+  const buts = [but_del, but_edit];
+
+  elem.checked
+    ? buts.forEach((but) => (but.disabled = false))
+    : buts.forEach((but) => {
+        but.disabled = true;
+        getElement("#checkbox1").checked = false;
+        getElement("#button-del-all").style.display = "none";
+      });
+}
+
+function checkTh() {
+  const elem = event.target;
+  if (elem.id == "checkbox1") {
+    elem.checked
+      ? Array.from(document.querySelectorAll('input[type="checkbox"]'))
+          .splice(1)
+          .forEach((but) => {
+            but.checked = true;
+            butAvai(but);
+            getElement("#button-del-all").style.display = "flex";
+          })
+      : Array.from(document.querySelectorAll('input[type="checkbox"]'))
+          .splice(1)
+          .forEach((but) => {
+            but.checked = false;
+            butAvai(but);
+            getElement("#button-del-all").style.display = "none";
+          });
+  }
+}
+
+function delAll() {
+  all = true;
+  openDel();
+  getElement(
+    "#war_text"
+  ).textContent = `Are you sure you want to delete all users`;
+}
 
 function addRow(new_student) {
   const main_tbody = getElement("#main-tbody");
@@ -128,7 +183,7 @@ function addRow(new_student) {
 
   elements.but_cont.className = "buttons-table";
   elements.circle_status.className = "status-circle";
-  if(new_student.status == "active"){
+  if (new_student.status == "active") {
     elements.circle_status.style.backgroundColor = "green";
   }
 
@@ -138,7 +193,9 @@ function addRow(new_student) {
   elements.del_but.id = "button-del";
   elements.input.id = elements.label.htmlFor = `checkbox${new_student.id}`;
   elements.input.type = "checkbox";
-  elements.input.onclick = checkTh;
+  elements.input.onclick = function (event) {
+    butAvai(event.target);
+  };
 
   elements.edit_but.append(elements.img_edit);
   elements.del_but.append(elements.img_del);
@@ -146,8 +203,10 @@ function addRow(new_student) {
 
   elements.del_but.dataset.studentId = new_student.id;
   elements.edit_but.dataset.studentId = new_student.id;
-  elements.del_but.onclick = delStudent;
+  elements.del_but.onclick = openDelOne;
   elements.edit_but.onclick = editStudent;
+  elements.del_but.disabled = true;
+  elements.edit_but.disabled = true;
 
   for (let i = 0; i < 7; i++) new_tr.append(createElem("td"));
 
@@ -178,7 +237,7 @@ function checkForm() {
   if (checkValidForm()) {
     const new_student = new Student(
       select_group.value,
-      f_name.value +  l_name.value,
+      f_name.value + " " + l_name.value,
       select_gender.value,
       input_date.value,
       "disabled"
@@ -187,29 +246,43 @@ function checkForm() {
     addRow(new_student);
     changeDisplayFlex(getElement("#wrapper-shadow"));
     changeDisplayFlex(getElement("#addStudent"));
+  } else {
+    alert("Form is invalid");
   }
 }
 
-function delStudent(event) {
-  const clickedElem = event.target;
+function delStudent() {
+  if (all) {
+    students.length = 0;
+    const table = document.querySelector("table");
+    const rows = table.querySelectorAll("tr");
 
-  if (clickedElem.id === "button-del") {
-    const studentId = clickedElem.dataset.studentId;
-    event.stopPropagation();
+    rows.forEach((row, index) => {
+      if (index !== 0) {
+        row.remove();
+      }
+    });
+    getElement("#checkbox1").checked = false;
+  } else {
+    if (clickedElemToDel.id === "button-del") {
+      const studentId = clickedElemToDel.dataset.studentId;
+      event.stopPropagation();
 
-    const rowToDelete = clickedElem.closest("tr");
-    rowToDelete.remove();
+      const rowToDelete = clickedElemToDel.closest("tr");
+      rowToDelete.remove();
 
-    const studentIndex = students.findIndex(
-      (student) => student.id == studentId
-    );
-    if (studentIndex !== -1) {
-      students.splice(studentIndex, 1);
-      console.log(`Student with ID: ${studentId} removed from the list`);
+      const studentIndex = students.findIndex(
+        (student) => student.id == studentId
+      );
+      if (studentIndex !== -1) {
+        students.splice(studentIndex, 1);
+        console.log(`Student with ID: ${studentId} removed from the list`);
+      }
     }
   }
-}
 
+  closeDel();
+}
 
 function editStudent() {
   const clickedElem = event.target;
@@ -268,16 +341,50 @@ function editRow(student) {
   }
 }
 
-function bell(){
-  document.getElementById('bell').animate([
-    { transform: 'rotate(0deg)' },
-    { transform: 'rotate(15deg)' },
-    { transform: 'rotate(-15deg)' },
-    { transform: 'rotate(10deg)' },
-    { transform: 'rotate(-10deg)' },
-    { transform: 'rotate(0deg)', offset: 0.5 }
-  ], {
-    duration: 1500,
-    iterations: 1
+document.addEventListener("DOMContentLoaded", function () {
+  const observer = new MutationObserver(() => {
+    let notif = document.getElementById("sign-not");
+    if (notif) {
+      observer.disconnect(); // Зупинити спостереження після знаходження
+      if (localStorage.getItem("notifHidden") === "true") {
+        notif.style.display = "none";
+      }
+    }
   });
+  observer.observe(document.body, { childList: true, subtree: true });
+});
+
+function hideNotif(event) {
+  console.log(event.target);
+  let notif = getElement("#sign-not");
+  if (notif) {
+    notif.style.display = "none";
+    localStorage.setItem("notifHidden", "true");
+  }
+}
+
+function bell() {
+  document
+    .getElementById("bell")
+    .animate(
+      [
+        { transform: "rotate(0deg)" },
+        { transform: "rotate(15deg)" },
+        { transform: "rotate(-15deg)" },
+        { transform: "rotate(10deg)" },
+        { transform: "rotate(-10deg)" },
+        { transform: "rotate(0deg)", offset: 0.5 },
+      ],
+      {
+        duration: 1500,
+        iterations: 1,
+      }
+    );
+  localStorage.setItem("notifHidden", "false");
+  let notif = getElement("#sign-not");
+  notif.style.display = "block";
+}
+
+function okClick() {
+  checkValidForm() ? checkForm() : closeAdd();
 }
